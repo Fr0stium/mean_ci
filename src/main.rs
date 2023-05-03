3e5e575d5070063e4fd56ef2f779af497d52e642
02Naitsirk <iifrostii@outlook.com>
2023-05-03 18:14:12 -0400

    Rename variables and add comments

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,10 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-const MIN_SUPPORT: f64 = 0.;
-const MAX_SUPPORT: f64 = 100.;
+const MIN_SUPPORT: f64 = 0.; // Minimum possible value of the dataset.
+const MAX_SUPPORT: f64 = 100.; // Maximum possible value of the dataset.
 
-/// Converts the data in data.txt to a vector of floats. data.txt must have a format of comma-separated numbers.
+/// Converts the data in "data.txt" to a vector of floats. "data.txt" must have a format of comma-separated numbers.
 fn get_ratings() -> Vec<f64> {
     let file = match File::open("data.txt") {
         Ok(file) => file,
@@ -37,7 +37,7 @@ fn get_ratings() -> Vec<f64> {
 }
 
 /// Gets the 100(1-α)% confidence interval for the mean of the dataset.
-fn get_confidence_interval(alpha: f64) -> (f64, f64) {
+fn get_mean_ci(alpha: f64) -> (f64, f64) {
     let ratings = get_ratings();
     let n = ratings.len() as f64;
 
@@ -60,34 +60,35 @@ fn get_confidence_interval(alpha: f64) -> (f64, f64) {
         }
     }
 
+    // Gets the 100(1-α)% confidence interval for a particular value of the CDF.
     let get_cdf_ci = |x: f64| -> (f64, f64) {
         let ratings_below_x = ratings.iter().filter(|&&rating| rating <= x).count() as f64;
-        let (mut lower_cdf_ci, mut upper_cdf_ci) = (0., 1.);
+        let (mut cdf_lower_ci, mut cdf_upper_ci) = (0., 1.);
 
         if x >= min {
-            lower_cdf_ci = incbi(ratings_below_x, n - ratings_below_x + 1., alpha / 2.);
+            cdf_lower_ci = incbi(ratings_below_x, n - ratings_below_x + 1., alpha / 2.);
         }
 
         if x < max {
-            upper_cdf_ci = incbi(1. + ratings_below_x, n - ratings_below_x, 1. - alpha / 2.);
+            cdf_upper_ci = incbi(1. + ratings_below_x, n - ratings_below_x, 1. - alpha / 2.);
         }
 
-        (lower_cdf_ci, upper_cdf_ci)
+        (cdf_lower_ci, cdf_upper_ci)
     };
 
-    let mut lower_ci_sum = 0.;
-    let mut upper_ci_sum = 0.;
+    let mut cdf_lower_ci_sum = 0.;
+    let mut cdf_upper_ci_sum = 0.;
 
     for i in 0..unique_ratings.len() - 1 {
         let (lower_cdf_ci, upper_cdf_ci) = get_cdf_ci(unique_ratings[i]);
-        lower_ci_sum += (unique_ratings[i + 1] - unique_ratings[i]) * upper_cdf_ci;
-        upper_ci_sum += (unique_ratings[i + 1] - unique_ratings[i]) * lower_cdf_ci;
+        cdf_lower_ci_sum += (unique_ratings[i + 1] - unique_ratings[i]) * upper_cdf_ci;
+        cdf_upper_ci_sum += (unique_ratings[i + 1] - unique_ratings[i]) * lower_cdf_ci;
     }
 
-    let lower_ci = max - (min - MIN_SUPPORT) * get_cdf_ci(MIN_SUPPORT).1 - lower_ci_sum;
-    let upper_ci = MAX_SUPPORT - (MAX_SUPPORT - max) * get_cdf_ci(max).0 - upper_ci_sum;
+    let mean_lower_ci = max - (min - MIN_SUPPORT) * get_cdf_ci(MIN_SUPPORT).1 - cdf_lower_ci_sum;
+    let mean_upper_ci = MAX_SUPPORT - (MAX_SUPPORT - max) * get_cdf_ci(max).0 - cdf_upper_ci_sum;
 
-    (lower_ci, upper_ci)
+    (mean_lower_ci, mean_upper_ci)
 }
 
 fn main() {
@@ -96,7 +97,7 @@ fn main() {
 
     let n = get_ratings().len();
     let mean = get_ratings().iter().sum::<f64>() / (n as f64);
-    let confidence_interval = get_confidence_interval(alpha);
+    let confidence_interval = get_mean_ci(alpha);
 
     println!("Number of ratings: {n}\nMean: {mean}\n{confidence_level}% confidence interval: {confidence_interval:?}")
 }
